97e9e1c609b130ca484121af6d983a05281fe4de
maria-manuel <mariamanuel.tech@gmail.com>
2023-11-29 21:35:03 -0800

    wfile contact-me page

--- 2_tcp_to_http/tcp_http_server.py
+++ 2_tcp_to_http/tcp_http_server.py
@@ -21,13 +21,17 @@ class RequestHandler(socketserver.StreamRequestHandler):
             self.wfile.write(b'<p><a href="/about-me>Learn more about me</a></p>')
             self.wfile.write(b'<p><a href="/contact-me">Send me an email</a></p>')
         # If the request contains like "/about-me"
-        #    then reply with a bio page
+        #    then reply with a about me page
         elif request == "GET /about-me HTTP/1.1":
             self.wfile.write(b'HTTP/1.1 200 OK\n \n')
             self.wfile.write(b'<h1>About me!</h1>')
             self.wfile.write(b'<p>I am a coder</p>')
-        # If the request is something like "/about"
-        #    Then reply with an about page
+        # If the request is something like "/contact me"
+        #    Then reply with a contact page
+        elif request == "GET /contact-me HTTP/1.1":
+            self.wfile.write(b'HTTP/1.1 200 OK\n \n')
+            self.wfile.write(b'<h1>Contact</h1>')
+            self.wfile.write(b'<p>Wanna hear from you!</p>')
 
         # Just say the same thing to all requests, the 200 OK reply required by
         # the HTTP protocol, then the text "Hello server world!"
